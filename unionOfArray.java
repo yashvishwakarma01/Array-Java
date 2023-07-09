8369cc31836f5a4d0d4d5c8eaf4f76d30f133695
Yash Vishwakarma <yashvishwakarmaofficial@gmail.com>
2023-07-09 05:51:13 +0530

    Create unionOfArray.java

---
+++ unionOfArray.java
@@ -0,0 +1,26 @@
+
+
+//User function Template for Java
+
+//arr1,arr2 : the arrays
+// n, m: size of arrays
+class Solution
+{
+    //Function to return a list containing the union of the two arrays.
+    public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        ArrayList<Integer> list=new ArrayList<>();
+        TreeSet<Integer> set=new TreeSet<>();
+        for(int i=0;i<n;i++){
+            set.add(arr1[i]);
+        }
+        for(int i=0;i<m;i++){
+            set.add(arr2[i]);
+        }
+        for(int element:set){
+            list.add(element);
+        }
+        return list;
+    }
+}
+
